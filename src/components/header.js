606c6884c784603f4654cfefb525f9ac53fc562f
marknemenzo <mlnemenzo@gmail.com>
2018-06-22 17:42:43 -0700

    react-router-dom added. header component built. no functionality

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends Component {
+    render() {
+        <div>
+            <h1 className="header">Header</h1>
+        </div>
+    }
+}
+
+export default Header;
