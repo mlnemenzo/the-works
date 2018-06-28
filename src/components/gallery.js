ecd7ec6bbc98e1abc519070a5ea024d8598d025a
marknemenzo <mlnemenzo@gmail.com>
2018-06-28 13:12:38 -0700

    carousel bootstrap

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -13,7 +13,7 @@ class Gallery extends Component {
         return (
             <div className="gallery-body">
                 <h1 className="gallery-header">Gallery</h1>
-                    <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                    {/* <div id="myCarousel" className="carousel slide" data-ride="carousel">
                         <ol className="carousel-indicators">
                             <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
                             <li data-target="#myCarousel" data-slide-to="1"></li>
@@ -39,6 +39,33 @@ class Gallery extends Component {
                             <span className="glyphicon glyphicon-chevron-right"></span>
                             <span className="sr-only">Next</span>
                         </a>
+                </div> */}
+
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <img className="d-block w-100" src={cars} alt="First slide">
+                    </div>
+                    <div className="carousel-item">
+                    <img className="d-block w-100" src={car2} alt="Second slide">
+                    </div>
+                    <div className="carousel-item">
+                    <img className="d-block w-100" src={car3}alt="Third slide">
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
                 </div>
             </div>
         )
