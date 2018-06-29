ca67ca9fb64da77adaf15244db356edda8445302
marknemenzo <mlnemenzo@gmail.com>
2018-06-28 19:45:41 -0700

    react carousel added

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,63 +1,74 @@
 import React, { Component } from 'react';
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import { React_Bootstrap_Carousel } from 'react-bootstrap-carousel';
 import cars from '../assets/images/car.jpg';
 import car2 from '../assets/images/car2.jpg';
 import car3 from '../assets/images/car3.jpg';
 
-
+const styles = { height: 400, width: "100%" };
 
 class Gallery extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            autoplay : true
+        };
+
+        
+    }
+
+    onSelect = (active, direction) => {
+        console.log(`active=${active} && direction=${direction}`);
+      };
+      slideNext = () => {
+        this.slider.slideNext();
+      };
+      slidePrev = () => {
+        this.slider.slidePrev();
+      };
+      goToSlide = () => {
+        this.slider.goToSlide(4);
+      };
+      autoplay = () => {
+        this.setState({ autoplay: !this.state.autoplay });
+      };
+
+      _changeIcon = () => {
+        let { leftIcon, rightIcon } = this.state;
+        if (leftIcon && rightIcon) {
+          this.setState({
+            leftIcon: undefined,
+            rightIcon: undefined
+          });
+        } else {
+          this.setState({
+            leftIcon: <span className="fa fa-glass" />,
+            rightIcon: <span className="fa fa-music" />
+          });
+        }
+      };
+    
 
 
     render() {
+
+        let {leftIcon, rightIcon} = this.state;
+
         return (
             <div className="gallery-body">
-                <h1 className="gallery-header">Gallery</h1>
-                    {/* <div id="myCarousel" className="carousel slide" data-ride="carousel">
-                        <ol className="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                        </ol>
-                        <div className="carousel-inner">
-                            <div className="item active">
-                                <img src={cars}/>
-                            </div>
-                            <div className="item active">
-                                <img src={car2}/>
-                            </div>
-                            <div className="item active">
-                            <img src={car3}/>
-                            </div>                   
-                        </div>
-
-                        <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-                            <span className="glyphicon glyphicon-chevron-left"></span>
-                            <span className="sr-only">Previous</span>
-                        </a>
-                        <a className="right carousel-control" href="#myCarousel" data-slide="next">
-                            <span className="glyphicon glyphicon-chevron-right"></span>
-                            <span className="sr-only">Next</span>
-                        </a>
-                </div> */}
-
-                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
+                {/* <h1 className="gallery-header">Gallery</h1>
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
-                    <img className="d-block w-100" src={cars} alt="First slide">
-                    </div>
                     <div className="carousel-item">
-                    <img className="d-block w-100" src={car2} alt="Second slide">
-                    </div>
-                    <div className="carousel-item">
-                    <img className="d-block w-100" src={car3}alt="Third slide">
+                        <img className="d-block w-100" src={cars} alt="First slide"/>
                     </div>
                 </div>
+                <div className="carousel-item">
+                    <img className="d-block w-100" src={car2} alt="Second slide"/>
+                </div>
+                <div className="carousel-item active">
+                    <img className="d-block w-100" src={car3}alt="Third slide"/>
+                </div>               
                 <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
@@ -65,9 +76,93 @@ class Gallery extends Component {
                 <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
-                </a>
+                </a>  */}
+                <div className="row">
+                    <div className="col-md-12" style={{ marginTop: 20 }}>
+                        <div className="btn-group">
+                            <button type="button" className="btn btn-primary" onClick={this._changeIcon}>
+                                Change Icon
+                            </button>
+                            <button type="button" className="btn btn-primary" onClick={this.slidePrev}>
+                                Slider prev
+                            </button>
+                            <button type="button" className="btn btn-primary" onClick={this.slideNext}>
+                                Slider next
+                            </button>
+                            <button type="button" className="btn btn-primary" onClick={this.goToSlide}>
+                                Go to slide 4
+                            </button>
+                            <button type="button" className="btn btn-primary" onClick={this.autoplay}>
+                                {this.state.autoplay ? "Stop Autoplay" : "Start Autoplay"}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="col-md-12" style={{ marginTop: 20 }}>
+                        <React_Bootstrap_Carousel
+                        animation={true}
+                        autoplay={this.state.autoplay}
+                        slideshowSpeed={7000}
+                        leftIcon={leftIcon}
+                        rightIcon={rightIcon}
+                        onSelect={this.onSelect}
+                        ref={r => (this.slider = r)}
+                        version={4}
+                        >              <div style={{ height: 400 }}>
+                        <img
+                          style={{ width: "100%", height: "100%" }}
+                          src={cars}
+                        />
+                        <div className="carousel-caption">Image</div>
+                      </div>
+                      <div style={{ ...styles, backgroundColor: "aqua" }}>
+                        <video className="carousel-center" controls style={{ width: "75%" }} height="250">
+                          <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+                        </video>
+                        <div className="carousel-caption">Video</div>
+                      </div>
+                      <div
+                        style={{
+                          height: 400,
+                          width: "100%",
+                          backgroundColor: "lightpink"
+                        }}
+                      >
+                        <div className="carousel-center">center Text</div>
+                        <div className="carousel-caption">Text</div>
+                      </div>
+                      <div style={{ ...styles, backgroundColor: "lightblue" }}>
+                        <span>text</span>
+                        <div className="carousel-caption">Text</div>
+                      </div>
+                      <div style={{ ...styles, backgroundColor: "lightblue" }}>
+                        <div className="carousel-center">
+                          <iframe
+                            style={{ width: 500 }}
+                            height="250"
+                            src="https://www.youtube.com/embed/MhkGQAoc7bc?showinfo=0"
+                            frameBorder="0"
+                            allowFullScreen
+                          />
+                        </div>
+                        <div className="carousel-caption">Youtube</div>
+                      </div>
+                    </React_Bootstrap_Carousel>
+                  </div>
+                  <div className="col-md-12" style={{ marginTop: 20 }}>
+                    <React_Bootstrap_Carousel version={4} className="carousel-fade">
+                      <div style={{ ...styles, backgroundColor: "darkcyan" }}>
+                        <div className="carousel-center">This carsouel transition is fade</div>
+                        <div className="carousel-caption">Text</div>
+                      </div>
+                      <div style={{ ...styles, backgroundColor: "yellowgreen" }}>
+                        <span className="carousel-center">This carsouel transition is fade</span>
+                        <div className="carousel-caption">Text</div>
+                      </div>
+                    </React_Bootstrap_Carousel>
                 </div>
+
             </div>
+         </div>   
         )
     }
 }
