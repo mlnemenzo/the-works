832b3cb8119af99e702aa21e80180fde04a461a7
marknemenzo <mlnemenzo@gmail.com>
2018-07-01 04:18:17 -0700

    styling break

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header';
@@ -13,26 +14,43 @@ import Disclaimer from './components/disclaimer';
 import Footer from './components/footer';
 
 
-const App = () => {
+class App extends Component {
 
-    return (
-      <div className="App">
-        <Header />
-          <div className = "page-header"></div>
-          <div className="container main-app-body">
-              <Route exact path="/" component={LandingPage}/>
-              <Route path ="/about-us" component={AboutUs}/>
-              <Route path ="/appointment" component={Appointment}/>
-              <Route path ="/contact" component={Contact}/>
-              <Route path ="/gallery" component={Gallery}/>
-              <Route path ="/payments" component={Payments}/>
-              <Route path ="/services" component={Services}/>
-              <Route path ="/disclaimer" component={Disclaimer}/>
-          </div>
-        <Footer />
-      </div>
-    );
+  constructor(props) {
+    super(props)
+
+      this.state = {
+        users : [],
+        store : []
+      }
+  }
+
+  componentDidMount() {
+    axios.get('https://randomuser.me/api/?results=10&inc=name,registered&nat=fr')
+    .then(json => console.log(json.data.results[5].name.title));
+    
+  }
+
+  render() {
+  return (
+    <div className="App">
+      <Header />
+        <div className = "page-header"></div>
+        <div className="container main-app-body">
+            <Route exact path="/" component={LandingPage}/>
+            <Route path ="/about-us" component={AboutUs}/>
+            <Route path ="/appointment" component={Appointment}/>
+            <Route path ="/contact" component={Contact}/>
+            <Route path ="/gallery" component={Gallery}/>
+            <Route path ="/payments" component={Payments}/>
+            <Route path ="/services" component={Services}/>
+            <Route path ="/disclaimer" component={Disclaimer}/>
+        </div>
+      <Footer />
+    </div>
+  );
   
+  } 
 }
 
 export default App;
