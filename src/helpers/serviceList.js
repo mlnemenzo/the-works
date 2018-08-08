5ff0770e166ecf174e94da17f6ee7191d7e6ee47
marknemenzo <mlnemenzo@gmail.com>
2018-08-08 00:03:08 -0700

    creating array for extra services

---
+++ src/helpers/serviceList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default [
+    
+    {item :'Engine Detail - $55.00'}, 
+    {item :'Headlight Restoration - $55.00'},
+    {item : 'Aluminum and/or Chrome Polish - $35.00'}, 
+    {item : 'Concentrated Scratch Correction - $55.00/Panel'},
+    {item : 'Water Spots Removal - $55.00/Panel'},
+    {item : 'Trim & Plastic Coating - $55.00'}, 
+    {item : 'Fabric & Leather Protection & Restoration (Interior & Exterior) - $55.00'}, 
+    {item : 'Ozone Decontamination - $75.00'}, 
+    {item : 'Glass Polishing and Restoration - $75.00'},
+    {item : 'R1 Ceramic Coating - $650.00' }]
