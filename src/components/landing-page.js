7ca04bf89504fe966639c0b0af148d49caecfe4b
marknemenzo <mlnemenzo@gmail.com>
2018-08-10 05:01:09 -0700

    end of day

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -16,10 +16,6 @@ class LandingPage extends Component {
                 <div className="landing-image justify-content-center">
                     <img src={landing} alt = "img not available"/>
                 </div>
-                <div className="appt">
-                    <h3 className="make-appt text-center">MAKE AN APPOINTMENT NOW WITH OUR ONLINE FORM:</h3>
-                    <Link className="make-appt-link nav-link nav-link-text boldLink" to = "/appointment">MAKE AN APPOINTMENT</Link>
-                </div>
                 <div className="why-choose col-10 offset-1">
                     <h2>WHY CHOOSE US?</h2>
                     <p>We are Certified Advance Auto Detailing and Auto Reconditioning Professionals.  We have extensive training and experience in the field of auto detailing, paint correction and R1 Ceramic Coatings.  We use eco-safe products and commercial grade equipment to ensure optimum results without harming the environment.  As an Auto Reconditioning Professional, we provide a systematic and efficient appropact to rejuvenate and protect various surfaces / components of your vehicle.</p>
@@ -68,6 +64,9 @@ class LandingPage extends Component {
                     <div className="service-images">
                     </div>
                     <Link className = "about-link nav-link" to = "/services">VIEW ALL SERVICES</Link> 
+                </div>
+                <div className="appt col-10 offset-1">
+                    <h4 className="make-appt text-center"><Link className="make-appt-link nav-link nav-link-text boldLink" to = "/appointment">MAKE AN APPOINTMENT NOW WITH  OUR ONLINE FORM.</Link></h4>
                 </div>
                 <div className="disclaimer-landing">
                     <h6>The Works Auto Detailing And Reconditioning LLC</h6>
