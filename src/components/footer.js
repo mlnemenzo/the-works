25e96ef9af4b0108a6f52835e43bb6b5d8478f9f
marknemenzo <mlnemenzo@gmail.com>
2018-09-03 03:48:24 -0700

    form validation build

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
     render() {
         return(
             <div className="footer-body row">
                 <div className="contact-email col-4">business email</div>
-                <div className="social-media col-4">media links</div>
+                <div className="social-media col-4">
+                    <h5 className="contact-us">contact us:</h5>
+                    <Link className = "contacdt-link text-center" to = "/contact">theworkscardetail@gmail.com</Link>
+                </div>
                 <div className="footer-number col-4">business number</div>
             </div>
         )
