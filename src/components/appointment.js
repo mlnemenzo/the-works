1d35e6cc2075211c831da2630ae696bffe336219
marknemenzo <mlnemenzo@gmail.com>
2018-09-24 04:55:39 -0700

    reworking mult select

--- src/components/appointment.js
+++ src/components/appointment.js
@@ -35,16 +35,16 @@ class Appointment extends Component {
                 time : "", 
                 date : new Date(),
                 minDate : new Date(),
-                items : [{_id: 1, item :'Engine Detail - $55.00'}, 
-                    {_id : 2, item :'Headlight Restoration - $55.00'},
-                    {_id : 3, item : 'Aluminum and/or Chrome Polish - $35.00'}, 
-                    {_id : 4, item : 'Concentrated Scratch Correction - $55.00/Panel'},
-                    {_id : 5, item : 'Water Spots Removal - $55.00/Panel'},
-                    {_id : 6, item : 'Trim & Plastic Coating - $55.00'}, 
-                    {_id : 7, item : 'Fabric & Leather Protection & Restoration (Interior & Exterior) - $55.00'}, 
-                    {_id : 8, item : 'Ozone Decontamination - $75.00'}, 
-                    {_id : 9, item : 'Glass Polishing and Restoration - $75.00'},
-                    {_id : 10, item : 'R1 Ceramic Coating - $650.00' }],
+                items : [{id: 1, item :'Engine Detail - $55.00'}, 
+                    {id : 2, item :'Headlight Restoration - $55.00'},
+                    {id : 3, item : 'Aluminum and/or Chrome Polish - $35.00'}, 
+                    {id : 4, item : 'Concentrated Scratch Correction - $55.00/Panel'},
+                    {id : 5, item : 'Water Spots Removal - $55.00/Panel'},
+                    {id : 6, item : 'Trim & Plastic Coating - $55.00'}, 
+                    {id : 7, item : 'Fabric & Leather Protection & Restoration (Interior & Exterior) - $55.00'}, 
+                    {id : 8, item : 'Ozone Decontamination - $75.00'}, 
+                    {id : 9, item : 'Glass Polishing and Restoration - $75.00'},
+                    {id : 10, item : 'R1 Ceramic Coating - $650.00' }],
                 selectedItems : []
                                                          
             }  
@@ -201,9 +201,9 @@ class Appointment extends Component {
                     <MultiSelect
                         items={items}
                         selectedItems={selectedItems}
-                        onClick = {()=> this.items(this.items.item)}
-                        showSearch ={true}
-                        showSelectedItems ={true}
+                        onChange={this.handleChange}
+                        showSearch ={false}
+                        showSelectedItems ={false}
                     />
                     </div>
                 </div>
