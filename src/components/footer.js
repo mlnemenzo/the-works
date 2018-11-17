6528ff8b2c3a4406c5367b0321a7c77e40b302a1
marknemenzo <mlnemenzo@gmail.com>
2018-11-17 02:17:18 -0800

    css clean up

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,10 +5,13 @@ class Footer extends Component {
     render() {
         return(
             <div className="footer-body row">
-                <div className="contact-email col-4">business email</div>
+                <div className="contact-email col-4">
+                    <h5>busines email</h5>
+                    <Link className = "contacdt-link text-center" to = "/contact">theworkscardetail@gmail.com</Link>
+                </div>
                 <div className="social-media col-4">
                     <h5 className="contact-us">contact us:</h5>
-                    <Link className = "contacdt-link text-center" to = "/contact">theworkscardetail@gmail.com</Link>
+                    
                 </div>
                 <div className="footer-number col-4">
                     <h5 className="number">business number</h5>
